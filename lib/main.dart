87fbf3bb4a1564192de90514c4c471e0ebdab1db
Parineeta-coder <parineeta.das11@gmail.com>
2021-06-17 18:37:25 +0530

    day 2 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/home_page.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context) 
+  {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text("Welcome to 8hrs course"),
-          ),
-        ),
-      ),
-    );
+      home: HomePage()
+      );
   }
 }
