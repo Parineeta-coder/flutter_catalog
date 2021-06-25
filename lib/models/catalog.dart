0c8d546a2e4234def5c6216fa53171601bb10ff3
Parineeta-coder <parineeta.das11@gmail.com>
2021-06-25 22:32:37 +0530

    day 12 completed

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -2,7 +2,7 @@
 import 'package:flutter/material.dart';
 
 class CatalogModel {
-  static final items = [
+  static late List<Item> items = [
     Item(
         id: 1,
         name: "iPhone 12 Pro",
@@ -11,22 +11,6 @@ class CatalogModel {
         color: "#33505a",
         image:
             "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRISJ6msIu4AU9_M9ZnJVQVFmfuhfyJjEtbUm3ZK11_8IV9TV25-1uM5wHjiFNwKy99w0mR5Hk&usqp=CAc"),
-    Item(
-        id: 2,
-        name: "iPhone 11 Pro",
-        desc: "Apple iPhone 11th generation",
-        price: 1001,
-        color: "#33505a",
-        image:
-            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRISJ6msIu4AU9_M9ZnJVQVFmfuhfyJjEtbUm3ZK11_8IV9TV25-1uM5wHjiFNwKy99w0mR5Hk&usqp=CAc"),
-    Item(
-        id: 1,
-        name: "iPhone 14 Pro",
-        desc: "Apple iPhone 12th generation",
-        price: 1400,
-        color: "#33505a",
-        image:
-            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRISJ6msIu4AU9_M9ZnJVQVFmfuhfyJjEtbUm3ZK11_8IV9TV25-1uM5wHjiFNwKy99w0mR5Hk&usqp=CAc")
   ];
 }
 
@@ -45,4 +29,24 @@ class Item {
       required this.price,
       required this.color,
       required this.image});
+
+  factory Item.fromMap(Map<String, dynamic> map) {
+    return Item(
+      id: map['id'],
+      name: map['name'],
+      desc: map['desc'],
+      price: map['price'],
+      color: map['color'],
+      image: map['image'],
+    );
+  }
+
+  toMap() => {
+        'id': id,
+        'name': name,
+        'desc': desc,
+        'price': price,
+        'color': color,
+        'image': image,
+      };
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,33 +1,69 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_catalog/widgets/drawer.dart';
-import 'package:flutter_catalog/models/catalog.dart';
-import 'package:flutter_catalog/widgets/item_widget.dart';
-// import 'package:flutter/services.dart';
-// import 'dart:convert';
+import 'dart:convert';
 
-class HomePage extends StatelessWidget {
-  final int days = 30;
-  final String name = "Pari";
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import 'package:flutter_catalog/models/catalog.dart';
+import 'package:flutter_catalog/widgets/drawer.dart';
+import 'package:flutter_catalog/widgets/item_widget.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  // final int days = 30;
+
+  // final String name = "Pari";
+
+  @override
+  void initState() {
+    // ignore: todo
+    // TODO: implement initState
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    await Future.delayed(Duration(seconds: 2));
+    final catalogJson =
+        await rootBundle.loadString("assets/files/catalog.json");
+
+    final decodedData = jsonDecode(catalogJson);
+    var productsData = decodedData["products"];
+
+    CatalogModel.items = List.from(productsData)
+        .map<Item>((item) => Item.fromMap(item))
+        .toList();
+
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
-    final dummyList = List.generate(20, (index) => CatalogModel.items[0]);
+    // final dummyList = List.generate(20, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         title: Text("Catalog App"),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: ListView.builder(
-          itemCount: dummyList.length,
-          itemBuilder: (context, index) {
-            var itemWidget = ItemWidget(
-              item: dummyList[index],
-              key: new Key(index.toString()),
-            );
-            return itemWidget;
-          },
-        ),
+        // ignore: unnecessary_null_comparison
+        child: (CatalogModel.items != null && CatalogModel.items.isNotEmpty)
+            ? ListView.builder(
+                itemCount: CatalogModel.items.length,
+                itemBuilder: (context, index) {
+                  var itemWidget = ItemWidget(
+                    item: CatalogModel.items[index],
+                    key: new Key(index.toString()),
+                  );
+                  return itemWidget;
+                },
+              )
+            : Center(
+                child: CircularProgressIndicator(),
+              ),
       ),
       drawer: MyDrawer(),
     );
